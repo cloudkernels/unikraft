acecd41de9a36b0f7b90e9fbd08020731ef46b1a
Charalampos Mainas <charalampos.mainas@gmail.com>
2021-02-05 13:24:42 +0200

    Introduce vAccelrt library in Unikraft
    vAccel is a runtime system for hardware acceleration. It provides an API with
    a set of functions that the runtime is able to offload to hardware
    acceleration devices.
    
    In case of Unikraft vAccel will be used as a transport layer between the
    guest and the host, where the request will be offloaded in the relative
    hardware.
    
    As a result, in the case of Unikraft only the virtio plugin of vAccelrt is
    used.
    
    Signed-off-by: Charalampos Mainas <charalampos.mainas@gmail.com>

---
+++ lib/libvaccelrt/session.c
@@ -0,0 +1,41 @@
+#include <vaccel.h>
+#include <uk/print.h>
+#include <stdint.h>
+#include <sys/ioctl.h>
+//#include <virtio/accel.h>
+#include <accel.h>
+#include <string.h>
+
+#include "ioctl.h"
+#include "log.h"
+
+int virtio_sess_init(struct vaccel_session *sess, uint32_t flags __unused )
+{
+	struct accel_session session;
+
+	memset(&session, 0, sizeof(session));
+	int ret = dev_write(VACCEL_SESS_CREATE, &session);
+	if (ret)
+		return ret;
+
+	/* Write the session id back to user */
+	sess->session_id = session.id;
+
+	uk_pr_debug("session:%u New session\n", sess->session_id);
+
+	return VACCEL_OK;
+}
+
+int virtio_sess_free(struct vaccel_session *sess)
+{
+	struct accel_session session;
+	int ret;
+
+	memset(&session, 0, sizeof(session));
+	session.id = sess->session_id;
+	ret = dev_write(VACCEL_SESS_DESTROY, &session);
+
+	vaccel_debug("session:%u Free session\n", sess->session_id);
+
+	return ret;
+}
