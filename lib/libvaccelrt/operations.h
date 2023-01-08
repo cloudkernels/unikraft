9fa88fc7b08bba428e85de120da76f4c1968717a
Charalampos Mainas <cmainas@nubificus.co.uk>
2023-01-08 15:36:30 +0000

    Introduce sgemm - blas operation
    There is an incompatibility in the number of arguments between the
    vaccel_sgemm function in vaccelrt and virtio_sgemm in virtio plugin.
    The difference is two arguments, alpha and beta, which were introduced
    in vaccelrt.
    
    For the time being, we pass as many arguments as in the vaccelrt
    version. We might need to revisit this in the future.
    
    Signed-off-by: Charalampos Mainas <cmainas@nubificus.co.uk>

--- lib/libvaccelrt/operations.h
+++ lib/libvaccelrt/operations.h
@@ -7,10 +7,13 @@
 struct vaccel_session;
 
 int virtio_noop(struct vaccel_session *sess);
-int virtio_sgemm(struct vaccel_session *sess,
-		uint32_t k, uint32_t m, uint32_t n,
-		size_t len_a, size_t len_b, size_t len_c,
-		float *a, float *b, float *c);
+int virtio_sgemm(struct vaccel_session *sess, long long int m, long long int n,
+		long long int k, float alpha, float *a, long long int lda, float *b,
+		long long int ldb, float beta, float *c, long long int ldc);
+//int virtio_sgemm(struct vaccel_session *sess,
+//		uint32_t k, uint32_t m, uint32_t n,
+//		size_t len_a, size_t len_b, size_t len_c,
+//		float *a, float *b, float *c);
 int virtio_image_classification(struct vaccel_session *sess, void *img,
 		unsigned char *out_text, unsigned char *out_imgname,
 		size_t len_img,	size_t len_out_text, size_t len_out_imgname);
