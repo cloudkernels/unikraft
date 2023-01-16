90b16aaf97ff627d6c9837273f810a6f67835172
Charalampos Mainas <cmainas@nubificus.co.uk>
2023-01-16 18:45:46 +0000

    Enable image pose op
    Enable the use of image pose operation through genop
    
    Signed-off-by: Charalampos Mainas <cmainas@nubificus.co.uk>

--- lib/libvaccelrt/ops/genop.c
+++ lib/libvaccelrt/ops/genop.c
@@ -40,7 +40,7 @@ unpack_func_t callbacks[VACCEL_FUNCTIONS_NR] = {
 	vaccel_image_classification_unpack,	/* 2 */
 	vaccel_image_detection_unpack,		/* 3 */
 	vaccel_image_segmentation_unpack,	/* 4 */
-	//vaccel_image_pose_unpack,		/* 5 */
+	vaccel_image_pose_unpack,		/* 5 */
 	//vaccel_image_depth_unpack,		/* 6 */
 	//vaccel_exec_unpack,			/* 7 */
 	//vaccel_noop_unpack,			/* 8 */
